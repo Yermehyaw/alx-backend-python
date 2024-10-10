6cc20a254b819dc7a3790bff5b62a0805d5ebfab
Yermehyaw <yermehyaw@gmail.com>
2024-10-10 23:45:43 +0100

    Add more docs

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -6,5 +6,10 @@ Modules Imported: None
 
 
 def concat(str1: str, str2: str) -> str:
-    """Return the concat of two str in an annotated func"""
+    """Return the concat of two str in an annotated func
+
+    Args:
+    str1(str): string to be concated to
+    str2(str): string to be conacted with
+    """
     return str1 + str2
