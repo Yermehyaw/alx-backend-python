0229ad5c7d54c7eca759e09e42823c332e42b989
Yermehyaw <yermehyaw@gmail.com>
2024-10-11 00:02:01 +0100

    Add docs

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -7,6 +7,13 @@ Modules Imported: None
 
 sum: float = 0.0
 def sum_list(input_list: list[float]) -> float:
+    """
+    Returns sum of floats
+
+    Args:
+    input_list(list): list of floats
+
+    """
     for elem in input_list:
         sum += elem
     return sum
