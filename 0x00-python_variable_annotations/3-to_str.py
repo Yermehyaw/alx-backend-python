d7841a1b0ada5b2a1b0033c961162f68f810072d
Yermehyaw <yermehyaw@gmail.com>
2024-10-10 23:52:33 +0100

    Annotate a float to str func

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Modules Imported: None
+
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Return a stringified float
+
+    Arg:
+    n(float): float arg
+
+    """
+    return str(n)
