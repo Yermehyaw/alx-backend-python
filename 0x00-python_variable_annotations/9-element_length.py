3ad51ff227b59d09440001c330ffd3d962336481
Yermehyaw <yermehyaw@gmail.com>
2024-10-11 04:10:49 +0000

    Use abstract types to annotate a func

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Duck type parameters an dreturn values of a function
+
+Modules Imported: typing
+
+typing: Provides custom data types
+
+"""
+from typing import Iterator
+from typing import List
+from typing import Sequence
+from typing import Tuple
+
+
+def element_length(lst: Iterator[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Utilize abstract types for annotating a func
+
+    Args:
+    lst(Iterable): Any iterable object
+
+    Return:
+    An abstract list of abstract tuple of a sequence
+    """
+    return [(i, len(i)) for i in lst]
