70525da78848465d56b4cedb9863eb4fac212643
Yermehyaw <yermehyaw@gmail.com>
2024-10-10 20:29:38 -0700

    Fix mypy issues

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -7,5 +7,5 @@ typing: Provide types for py and earlier
 from typing import Union
 
 
-def to_kv(k: str, v: Union[int | float]) -> tuple:
-    return tuple(k, v)
+def to_kv(k: str, v: Union[int, float]) -> tuple:
+    return tuple([k, v])
