2facf56e6aaa9f0db9600b29928b30f0e70cb43b
Yermehyaw <yermehyaw@gmail.com>
2024-10-11 04:42:49 +0000

    Task req: Use typing module

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 """
-Modules Imported: None
+Modules Imported: typing
+
+typing: access default ans non-default hint types
 
 """
+from typing import List
 
 
-def sum_list(input_list: list) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     Returns sum of floats
 
