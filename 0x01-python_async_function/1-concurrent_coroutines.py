5e63a4e2dcbf9bd9e5e52b23ad2ae211777d060b
Yermehyaw <yermehyaw@gmail.com>
2024-10-15 04:13:18 +0000

    A coroutine that waits on a loop pf coroutines

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -17,4 +17,12 @@ import random
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n() -> list:
+async def wait_n(n: int, max_delay: int) -> list:
+    """Return a list of all random return times"""
+    delays = []
+    for i in range(n):
+        d_n = asyncio.create_task(wait_random(max_delay))
+        delay = await d_n
+        delays.append(delay)
+
+    return delays
