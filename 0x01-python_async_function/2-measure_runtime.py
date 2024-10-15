b23f8a001680d4a102d6a977c7737aaab8ab4a59
Yermehyaw <yermehyaw@gmail.com>
2024-10-15 05:01:05 +0000

    Evaluate time elaspsed in calling several coroutines

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Measure the runtime of multiple coroutines
+
+Modules imported: asyncio
+
+asyncio:
+    api module for IO operations using async concurrency
+random:
+    generate random ints
+time:
+    provides chronlogical interfaces
+wait_n:
+    runs multiple coroutines
+
+"""
+import asyncio
+import random
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """Measures execution time per coroutine awaited in the wait_n() func"""
+    start = time.perf_counter()
+    await wait_n(n, max_delay)
+    end = time.perf_counter()
+    elasped = end - start
+
+    return elasped/n
