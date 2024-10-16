bcae1e55b82613daea099ad25c8d764bb1cae376
Yermehyaw <yermehyaw@gmail.com>
2024-10-16 04:47:40 +0000

    An async list comprehension

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,25 @@
+#!/usr/bin env python3
+"""
+Forms a list via async comprehension
+
+Modules imported:
+asyncio: use async operation in python
+random: generate random values
+typing: custom types for python annotation
+
+"""
+import asyncio
+import random
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Produces a list from an async gen using list
+    comprehension
+
+    Returns:
+    A list of floats
+    """
+    return [rand async for rand in async_generator()]
