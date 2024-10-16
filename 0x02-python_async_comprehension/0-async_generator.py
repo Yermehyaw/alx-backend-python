ccdb2731afd28957b382aa17b2206b7de99f917c
Yermehyaw <yermehyaw@gmail.com>
2024-10-16 04:28:14 +0000

    Fix annotation

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -13,7 +13,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[int, None, None]:
+async def async_generator() -> Generator[float, None]:
     """An async geb func, yields a random number"""
     for _ in range(10):
         await asyncio.sleep(1)
