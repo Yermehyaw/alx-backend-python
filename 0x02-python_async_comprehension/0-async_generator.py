7c5e1280415e1683ec40ab99a4ea8585fc71612e
Yermehyaw <yermehyaw@gmail.com>
2024-10-16 04:30:18 +0000

    Add docs

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -14,7 +14,12 @@ from typing import Generator
 
 
 async def async_generator() -> Generator[float, None]:
-    """An async geb func, yields a random number"""
+    """
+    An async geb func, yields a random number
+
+    yields:
+    A random float between 0 and 10
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         rand = random.uniform(0, 10)
