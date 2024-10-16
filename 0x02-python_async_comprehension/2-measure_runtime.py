964a0cb1fdc766046f1a22b0b01f6b91374f9f8d
Yermehyaw <yermehyaw@gmail.com>
2024-10-16 04:55:18 +0000

    Measure runtime

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,32 @@
+#!/usr/bin env python3
+"""
+Measures the runtime of an asynchronous list comp
+
+Modules imported:
+asyncio: use async operation in python
+random: generate random values
+typing: custom types for python annotation
+
+"""
+import asyncio
+import random
+import time
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def measure_runtime() -> float:   
+    """
+    Measures runtime
+
+    Returns:
+    A float rep time elasped
+    """
+    start = time.perf_counter()
+    asyncio.gather(
+        async_generator(),
+        async_generator(),
+        async_generator(),
+        async_generator()
+    )
+    end = time.perf_counter()
+    return (end - start)
