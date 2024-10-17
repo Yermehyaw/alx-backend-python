78af03b92a0e3b9bd8001db196ed78c68e8394e2
Yermehyaw <yermehyaw@gmail.com>
2024-10-17 16:57:15 +0000

    Task req

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -8,7 +8,7 @@ random: generate random values
 typing: custom types for python annotation
 
 """
-import asyncio
+from asyncio import gather
 import random
 import time
 a_compr = __import__('1-async_comprehension').async_comprehension
@@ -22,6 +22,6 @@ async def measure_runtime() -> float:
     A float rep time elasped
     """
     start = time.perf_counter()
-    values = await asyncio.gather(a_compr(), a_compr(), a_compr(), a_compr())
+    values = await gather(a_compr(), a_compr(), a_compr(), a_compr())
     end = time.perf_counter()
     return (end - start)
