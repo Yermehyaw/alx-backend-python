cac2cbd1b890fd0cfb306e69004bb6cd9dacc56a
Yermehyaw <yermehyaw@gmail.com>
2024-10-17 15:56:37 +0000

    Fix async time based return & func annotation

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -14,17 +14,22 @@ wait_random:
 """
 import asyncio
 import random
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Return a list of all random return times"""
     delays = []
-    for i in range(n):
-        d_n = await wait_random(max_delay)
-        # delay = await d_n
-        delays.append(d_n)
+    task_objs = []
 
-    # delay_time = []
-    # delay_time = (await asyncio.gather(*delays))
+    for _ in range(n):
+        task_objs.append(asyncio.create_task(wait_random(max_delay)))
+
+    try:
+        delays = [await finsh for finsh in asyncio.as_completed(task_objs)]
+    except Exception as e:
+        print(f'Async event loop was unexpectedly interrupted: {e}')
+        # a graceful event loop closure is also supppsed to be here.
+
     return delays
