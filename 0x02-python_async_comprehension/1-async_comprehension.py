a3d515cee5f9f642cdb7c92f23aedbd173f335b9
Yermehyaw <yermehyaw@gmail.com>
2024-10-17 16:41:14 +0000

    Fix shebang

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,4 +1,4 @@
-#!/usr/bin env python3
+#!/usr/bin/env python3
 """
 Forms a list via async comprehension
 
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,4 +1,4 @@
-#!/usr/bin env python3
+#!/usr/bin/env python3
 """
 Measures the runtime of an asynchronous list comp
 
