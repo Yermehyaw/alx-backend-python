f7f755b67b5d2897dce02af058757faeffc94b81
Yermehyaw <yermehyaw@gmail.com>
2024-11-06 04:08:15 +0000

    Test3: Add mock test using patching on get_json()

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,17 +2,27 @@
 """
 IIntegration tests using unittest module
 
-Modules imported: utils, unittest, parameterized, typong
+Modules imported: utils, unittest, patch,  parameterized,
+rrequests, typing
 
 """
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import (
+    access_nested_map,
+    get_json
+)
 from typing import (
     Any,
+    Dict,
     Mapping,
     Sequence
 )
+import requests
 import unittest
+from unittest.mock import (
+    Mock,
+    patch
+)
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -54,3 +64,32 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(exception_raised) as e:
             access_nested_map(nested_map, path)
             self.assertEqual(str(e.exception), exception_msg)
+
+
+class TestGetJson(unittest.TestCase):
+    """Tests the get_json mthd in utils
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('requests.get')
+    def test_get_json(
+            self,
+            test_url: str,
+            test_package: Dict,
+            mock_obj: Mock,
+    ) -> None:
+        """Test get_json
+        """
+        # Create a response
+        mock_obj.return_value.json.return_value = test_package
+
+        # Make a request with the patched method
+        resp = get_json(test_url)
+
+        # Test if the patched mthd was called
+        mock_obj.assert_called_once_with(test_url)
+
+        # Test if the resp received is the same as the created response
+        self.assertEqual(test_package, resp)
